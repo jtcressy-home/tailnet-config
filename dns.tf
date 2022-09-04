91bc3c0ba994a30f9c8b1f86f38bd32166121faa
Joel Cressy <jcressy@vmware.com>
2022-09-04 03:49:33 +0000

    switch to google dns

--- dns.tf
+++ dns.tf
@@ -1,8 +1,8 @@
 resource "tailscale_dns_nameservers" "this" {
-  nameservers = concat(
-    data.dns_a_record_set.cloudflare_dns.addrs,
-    data.dns_aaaa_record_set.cloudflare_dns.addrs
-  )
+  nameservers = [
+    "8.8.8.8",
+    "8.8.4.4"
+  ]
 }
 
 resource "tailscale_dns_preferences" "this" {
