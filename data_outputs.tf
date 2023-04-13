f820b7edbe2d7cba289c0af93bf51c1cc28c5898
Joel Cressy <joel@jtcressy.net>
2023-04-13 12:42:58 -0500

    fix: remove unnecessary output triggering false drift alerts (#28)

--- data_outputs.tf
+++ data_outputs.tf
@@ -1,9 +1,5 @@
 data "tailscale_devices" "all_devices" {}
 
-output "tailscale_devices" {
-  value = jsonencode(data.tailscale_devices.all_devices.devices)
-}
-
 data "dns_a_record_set" "cloudflare_dns" {
   host = "one.one.one.one."
 }
